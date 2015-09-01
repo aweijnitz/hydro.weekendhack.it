a4500a99e77e6a3bda5a21dd2b3312d60296e788
Anders Weijnitz <anders.weijnitz@gmail.com>
2015-09-01 22:03:58 +0200

    Switched to lightbox gallery

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,7 @@
 // jshint devel:true
 console.log('Enter main.js');
+
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
